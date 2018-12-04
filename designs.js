d10c8b973ffcb3b09ef6ec7b0ceeca2c4b0b1f69
Joseph Harburg <josephharburg@gmail.com>
2018-12-04 14:14:24 -0500

    Added title function

--- designs.js
+++ designs.js
@@ -3,8 +3,17 @@
 
 // When size is submitted by the user, call makeGrid()
 
-function makeGrid() {
+// function makeGrid() {
+//   const submitted = document.querySelector("submit")
+//   const Width = document.querySelector('inputWidth')
+//   const Height = document.querySelector('inputHeight')
+//   const colorchosen =
+// };
 
-// Your code goes here!
-
+const mytitle = document.querySelector('h1');
+const colortochange = document.querySelector('#colorPicker')
+function changetitle(evt) {
+  if (evt.target === mytitle) {
+    mytitle.style.color = colortochange.value;}
 }
+document.addEventListener ('click', changetitle);
