8b2bd465944ce0562a6dde7cbe9a241e67659634
Joseph Harburg <josephharburg@gmail.com>
2018-12-08 17:14:14 -0500

    Added grid and colorchanger

--- designs.js
+++ designs.js
@@ -3,45 +3,46 @@
 
 const myTitle = document.querySelector('h1'); //title of page
 const colorPicked = document.querySelector('#colorPicker');// for color colorpicked
-const canvas = document.querySelector('#pixelCanvas')
+const removebutton = document.querySelector('button[name="remove"]');
+const canvas = document.querySelector('#pixelCanvas');
+const table = document.querySelector('table')
 // When size is submitted by the user, call makeGrid()
-// function removebox(event) {
-//   if (event.target === document.querySelector('#remove'))
-//   const removeboxes = document.querySelectorAll('td')
-//   removeboxes.remove();
+// function removebox() {
+//   // const removeboxes = document.querySelectorAll('td');
+//   // const removerows = document.querySelectorAll('tr');
+//   const removetable = document.querySelector('tbody');
+//   // removeboxes.remove();
+//   // removerows.remove();
+//   removetable.remove();
 // }
-canvas.insertAdjacentHTML('afterbegin', '<tr></tr>');
+
 function makeGrid(evt) {
   evt.preventDefault();
-  const canvas = document.querySelector('#pixelCanvas');
-  const newBox = '<td></td>';
-  const newRow = '<tr></tr>';
-  const row = document.querySelector('#inputWidth');
-  const column = document.querySelector('#inputHeight');
-  const rows = document.querySelector('tr');
-  for (var y = 0; y < obj.column.value; y++){
-    canvas.insertAdjacentHTML('afterbegin', '<tr></tr>');}
-  for (var column = 0; column < obj.row.value;column++){
-    rows.insertAdjacentHTML('afterbegin', '<td></td>');}
-  // const obj = {
-  //   row: document.querySelector('#inputWidth'),
-  //   column: document.querySelector('#inputHeight'),
-  //   rows: document.querySelector('tr'),
-  //   grid: function(){
-  //     for (var y = 0; y < obj.column.value; y++){
-  //       canvas.insertAdjacentHTML('afterbegin', '<tr></tr>');}
-  //     for (var column = 0; column < obj.row.value;column++){
-  //       rows.insertAdjacentHTML('afterbegin', '<td></td>');};
-  //   }
-  //   obj.grid();
+  // removebox();
+  const obj = {
+    row: document.querySelector('#inputWidth'),
+    column: document.querySelector('#inputHeight')
+  }
+  function tbodyadd() {canvas.insertAdjacentHTML('afterbegin','<tbody></tbody>');}
+  tbodyadd();
+  const rows = document.querySelector('tbody');
+  const gridmake = [];
+  function add() {for (var e = 0; e < obj.column.value; e++){gridmake.splice(e,0,"<tr>");
+  gridmake.splice(e-1,0,'</tr>');
+    for (var x = 0; x < obj.row.value; x++){gridmake.splice(e+1,0,"<td></td>");}}
+  }
+  add();
+  rows.insertAdjacentHTML('afterbegin',gridmake.join(""));
 }
 
-
+function changeColor(event) {
+  if (event.target.nodeName.toLowerCase() === 'td') {
+    event.target.style.backgroundColor = colorPicked.value;}
+}
 // function changeTitle(evt) {
 //   if (evt.target === myTitle) {
 //     myTitle.style.color = colorPicked.value;}
 // }
-//
+canvas.addEventListener('click', changeColor, false);
 // document.addEventListener ('click', changeTitle);
 document.addEventListener ('submit', makeGrid);
-// document.addEventListener('click', removebox);
