62709bca31a08b4a45b15cd6380874b5f7d3fa4c
Joseph Harburg <josephharburg@gmail.com>
2018-12-11 15:02:01 -0500

    Edited so code is consistantly styled and easier to read

--- designs.js
+++ designs.js
@@ -1,43 +1,48 @@
-// Select color input
-// Select size input
+const myTitle = document.querySelector('h1'); // Title of page for added extra
+const colorPicked = document.querySelector('#colorPicker');// For color picked
+const canvas = document.querySelector('#pixelCanvas');// Selects canvas
 
-const myTitle = document.querySelector('h1'); //title of page for added extra
-const colorPicked = document.querySelector('#colorPicker');// for color colorpicked
-const canvas = document.querySelector('#pixelCanvas');// selects canvas
-
-function removeBoxes(){
+function removeBoxes(){ // This function resets the canvas
   const tbodyPresent = document.querySelector('tbody');
   if (tbodyPresent != null){
     tbodyPresent.remove()
   }
-}//this function resets the canvas
+}
 
-function makeGrid(evt) {
+function makeGrid(evt) { // Makes the Grid on the canvas
   evt.preventDefault();
   removeBoxes();
   canvas.insertAdjacentHTML('afterbegin', '<tbody></tbody>')
   const boxattributes = {
     row: document.querySelector('#inputWidth'),
     column: document.querySelector('#inputHeight'),
-    tbodys: document.querySelector('tbody')
+    tableBody: document.querySelector('tbody')
   }
-  const gridmake = [];
-  function add() {for (var e = 0; e < boxattributes.column.value; e++){gridmake.splice(e,0,"<tr>");
-  gridmake.splice(e-1,0,'</tr>');
-    for (var x = 0; x < boxattributes.row.value; x++){gridmake.splice(e+1,0,"<td></td>");}}
+  const gridMake = [];
+
+  function add() { // Add script to gridMake array then inserts it into table
+    for (var e = 0; e < boxattributes.column.value; e++){
+    gridMake.splice(e,0,"<tr>"); gridMake.splice(e-1,0,'</tr>');
+    for (var x = 0; x < boxattributes.row.value; x++){
+      gridMake.splice(e+1,0,"<td></td>");}
+    }
+    boxattributes.tableBody.insertAdjacentHTML('afterbegin',gridMake.join(""));
   }
   add();
-  boxattributes.tbodys.insertAdjacentHTML('afterbegin',gridmake.join(""));
-}//makes the Grid on the canvas
+}
 
-function changeColor(event) {
+function changeColor(event) { // Changes the box color when clicked on
   if (event.target.nodeName.toLowerCase() === 'td') {
-    event.target.style.backgroundColor = colorPicked.value;}
+    event.target.style.backgroundColor = colorPicked.value;
+  }
 }
-function changeTitle(evt) {
+
+function changeTitle(evt) { // Changes the Title to color picked when clicked
   if (evt.target === myTitle) {
-    myTitle.style.color = colorPicked.value;}
+    myTitle.style.color = colorPicked.value;
+  }
 }
+
 canvas.addEventListener('click', changeColor, false);
 myTitle.addEventListener ('click', changeTitle);
 document.addEventListener ('submit', makeGrid);
